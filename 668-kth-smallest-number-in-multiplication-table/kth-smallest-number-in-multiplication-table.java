748b1c2a35d2e9c1965c4dc19df4f2e6e000f55f
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2026-03-18 07:53:02 +0530

    Time: 12 ms (20.12%) | Memory: 42.1 MB (70.79%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
@@ -1,26 +1,29 @@
 class Solution {
-    public boolean isValid(int row,int col,int mid,int k){
-        int r = row,c=1;
-        int count=0;
-        while(r >= 1 && c <= col){
-            if((r*c) > mid) r--;
-            else{
+    public boolean isValid(int row, int col, int mid, int k) {
+        int r = row, c = 1;
+        int count = 0;
+        while (r >= 1 && c <= col) {
+            if ((r * c) > mid)
+                r--;
+            else {
                 count += r;
                 c++;
             }
         }
         return count >= k;
     }
+
     public int findKthNumber(int m, int n, int k) {
-        int start = 1, end = m*n;
-        int res = -1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(isValid(m,n,mid,k)) {
-                res = mid;
-                end = mid -1;
-            }else start = mid+1;
+        int start = 1, end = m * n;
+        // int res = -1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (isValid(m, n, mid, k)) {
+                // res = mid;
+                end = mid;
+            } else
+                start = mid + 1;
         }
-        return res;
+        return start;
     }
 }
