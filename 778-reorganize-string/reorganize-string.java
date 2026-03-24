e63732ed23151190d7692bb95d0772d0c4ec36a8
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2026-03-24 16:05:10 +0530

    Time: 7 ms (25.70%) | Memory: 43.1 MB (65.37%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.java
@@ -0,0 +1,51 @@
+class Pair{
+    public Integer freq;
+    public Character ch;
+
+    public Pair(Integer freq,Character ch){
+        this.freq=freq;
+        this.ch = ch;
+    }
+
+}
+
+class Solution {
+    public String reorganizeString(String s) {
+        Map<Character,Integer> freq = new HashMap<>();
+        for(char ch:s.toCharArray()){
+            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        }
+        // for(Map.Entry<Character,Integer> m:freq.entrySet()){
+        //     System.out.println(m.getKey() + "----" + m.getValue());
+        // }
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(
+            (a,b) ->{
+                if(a.freq != b.freq) return b.freq - a.freq;
+                return b.ch.compareTo(a.ch);
+            }
+        );
+        for(Map.Entry<Character,Integer> m:freq.entrySet()){
+            maxHeap.add(new Pair(m.getValue(),m.getKey()));
+        }
+        StringBuilder res = new StringBuilder();
+        int seat=0;
+        while(!maxHeap.isEmpty()){
+            Pair p = maxHeap.poll();
+            if(seat == 0 || res.charAt(seat-1) != p.ch ){
+                res.append(p.ch);
+                seat++;
+                p.freq--;
+                if(p.freq > 0) maxHeap.add(p);
+            }else{
+                if(maxHeap.isEmpty()) return "";
+                Pair p1 = maxHeap.poll();
+                res.append(p1.ch);
+                seat++;
+                p1.freq--;
+                if(p1.freq > 0) maxHeap.add(p1);
+                maxHeap.add(p);
+            }
+        }
+        return res.toString();
+    }
+}
