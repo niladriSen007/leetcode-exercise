863fad45b1dd97dde7d05d52731055b20e3224c7
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2026-03-24 16:07:15 +0530

    Time: 6 ms (34.37%) | Memory: 43.3 MB (36.34%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,9 +1,9 @@
-class Pair{
+class Pair {
     public Integer freq;
     public Character ch;
 
-    public Pair(Integer freq,Character ch){
-        this.freq=freq;
+    public Pair(Integer freq, Character ch) {
+        this.freq = freq;
         this.ch = ch;
     }
 
@@ -11,40 +11,41 @@ class Pair{
 
 class Solution {
     public String reorganizeString(String s) {
-        Map<Character,Integer> freq = new HashMap<>();
-        for(char ch:s.toCharArray()){
-            freq.put(ch,freq.getOrDefault(ch,0)+1);
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
         // for(Map.Entry<Character,Integer> m:freq.entrySet()){
         //     System.out.println(m.getKey() + "----" + m.getValue());
         // }
         PriorityQueue<Pair> maxHeap = new PriorityQueue<>(
-            (a,b) ->{
-                if(a.freq != b.freq) return b.freq - a.freq;
-                return b.ch.compareTo(a.ch);
-            }
-        );
-        for(Map.Entry<Character,Integer> m:freq.entrySet()){
-            maxHeap.add(new Pair(m.getValue(),m.getKey()));
+                (a, b) -> {
+                    if (a.freq != b.freq)
+                        return b.freq - a.freq;
+                    return b.ch.compareTo(a.ch);
+                });
+        for (Map.Entry<Character, Integer> m : freq.entrySet()) {
+            maxHeap.add(new Pair(m.getValue(), m.getKey()));
         }
         StringBuilder res = new StringBuilder();
-        int seat=0;
-        while(!maxHeap.isEmpty()){
+        int seat = 0;
+        while (!maxHeap.isEmpty()) {
             Pair p = maxHeap.poll();
-            if(seat == 0 || res.charAt(seat-1) != p.ch ){
+            if (seat == 0 || res.charAt(seat - 1) != p.ch) {
                 res.append(p.ch);
-                seat++;
                 p.freq--;
-                if(p.freq > 0) maxHeap.add(p);
-            }else{
-                if(maxHeap.isEmpty()) return "";
+            } else {
+                if (maxHeap.isEmpty())
+                    return "";
                 Pair p1 = maxHeap.poll();
                 res.append(p1.ch);
-                seat++;
                 p1.freq--;
-                if(p1.freq > 0) maxHeap.add(p1);
-                maxHeap.add(p);
+                if (p1.freq > 0)
+                    maxHeap.add(p1);
             }
+            if (p.freq > 0)
+                maxHeap.add(p);
+            seat++;
         }
         return res.toString();
     }
