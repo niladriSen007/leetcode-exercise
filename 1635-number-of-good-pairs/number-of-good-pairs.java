754bb6bd766e114454ac29c63b76301bbb106c85
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2026-07-29 20:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (62.29%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.java
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -13,10 +13,27 @@ class Solution {
         int freq[] = new int[101];
 
         for(int num:nums){
-            count+=freq[num]; //0 1 3 6
             freq[num]++;
         }
 
+        // formula = k(k-1)/2
+        // 1 element occurs 2 times = 1 pair avalable = 2*(2-1)/2 = 1
+        // 1 element occurs 3 times = 3 pair avalable = 3*(3-1)/2 = 3
+        // 1 element occurs 4 times = 6 pair avalable = 4*(4-1)/2 = 6
+        for(int k:freq){
+            count+= k*(k-1)/2;
+        }
+
         return count;
+
+        // int count=0;
+        // int freq[] = new int[101];
+
+        // for(int num:nums){
+        //     count+=freq[num]; //0 1 3 6
+        //     freq[num]++;
+        // }
+
+        // return count;
     }
 }
