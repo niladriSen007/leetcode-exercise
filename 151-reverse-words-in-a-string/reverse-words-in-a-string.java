21ca0618943db5c6a105cf8660bd8365303da130
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2026-07-29 19:30:20 +0530

    Time: 7 ms (63.66%) | Memory: 46.9 MB (13.05%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,15 +1,40 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] list = s.trim().split("\\s+");
-        int n=list.length;
-        int left=0,right=n-1;
-        while(left<right){
-            String temp = list[left];
-            list[left] = list[right];
-            list[right]  =temp;
-            left++;
-            right--;
+        // String[] list = s.trim().split("\\s+");
+        // int n=list.length;
+        // int left=0,right=n-1;
+        // while(left<right){
+        //     String temp = list[left];
+        //     list[left] = list[right];
+        //     list[right]  =temp;
+        //     left++;
+        //     right--;
+        // }
+        // return String.join(" ",list);
+
+        // Take string length
+        int length = s.length();
+        String result = "";
+
+        // while ' ' is coming keep incrementing 'i'
+        int i = 0;
+
+        while (i < length) {
+            while (i < length && s.charAt(i) == ' ')
+                i++;
+
+            int j = i + 1 > length ? i : i + 1;
+            while (j < length && s.charAt(j) != ' ')
+                j++;
+
+            String substring = s.substring(i, j);
+
+            if (substring.length() > 0)
+                result = result.length() > 0 ? substring + " " + result : substring;
+
+            i = j + 1;
         }
-        return String.join(" ",list);
+
+        return result;
     }
 }
